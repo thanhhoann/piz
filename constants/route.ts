f249261cc3c91e4581b63f85b88ab45e1a97ba37
thanhhoann <pdth456@gmail.com>
2024-08-03 18:01:08 +0700

    ✨ feat(route): add type routes

--- constants/route.ts
+++ constants/route.ts
@@ -1,21 +1,23 @@
+import type { Route } from "next";
+
 export type RouteProps = {
-	HOME: string;
-	SIGN_IN: string;
-	SIGN_UP: string;
-	PROFILE: string;
-	SEARCH: string;
-	ACTIVITY: string;
-	SAVED_POSTS: string;
-	SETTINGS: string;
+	HOME: Route;
+	SIGN_IN: Route;
+	SIGN_UP: Route;
+	PROFILE: Route;
+	SEARCH: Route;
+	ACTIVITY: Route;
+	SAVED_POSTS: Route;
+	SETTINGS: Route;
 };
 
 export const ROUTE: RouteProps = {
-	HOME: "/",
-	SIGN_IN: "/sign-in",
-	SIGN_UP: "/sign-up",
-	SEARCH: "/search",
-	ACTIVITY: "/activity",
-	SAVED_POSTS: "/saved_posts",
-	SETTINGS: "/settings",
-	PROFILE: "#",
+	HOME: "/" as Route,
+	SIGN_IN: "/sign-in" as Route,
+	SIGN_UP: "/sign-up" as Route,
+	SEARCH: "/search" as Route,
+	ACTIVITY: "/activity" as Route,
+	SAVED_POSTS: "/saved_posts" as Route,
+	SETTINGS: "/settings" as Route,
+	PROFILE: "#" as Route,
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	transpilePackages: ["lucide-react"],
+	experimental: {
+		typedRoutes: true,
+	},
 };
 
 export default nextConfig;
